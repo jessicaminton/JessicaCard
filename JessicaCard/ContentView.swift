fba3b2febbbfa8aac3059233c9999048543b25a9
Jessica Minton <jessminton2@gmail.com>
2022-09-28 12:03:01 -0400

    initial commit

--- JessicaCard/ContentView.swift
+++ JessicaCard/ContentView.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("headshot copy")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                    )
+                Text("Jessica Minton")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("IT Business Systems Analyst")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "(727) 460-6253", imageName: "phone.fill")
+                InfoView(text: "jessminton2@gmail.com", imageName: "envelope.fill")
+            }
+        }
+        
     }
 }
 
@@ -19,3 +42,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
