e4ef28bf6542df843725becfcfb272172390a17d
Jessica Minton <jessminton2@gmail.com>
2022-09-28 10:54:53 -0400

    Initial Commit

---
+++ JessicaCard/JessicaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  JessicaCardApp.swift
+//  JessicaCard
+//
+//  Created by Jessica Minton on 9/28/22.
+//
+
+import SwiftUI
+
+@main
+struct JessicaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
